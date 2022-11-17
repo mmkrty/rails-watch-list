e4512cd614906b09a875c4ab7b52e2a1cb2a8501
haochenliu <mmkrty@gmail.com>
2022-11-17 14:38:00 +0100

    raked all green

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,45 @@
+class BookmarksController < ApplicationController
+
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def update
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.update(bookmark_params)
+    redirect_to bookmark_path(@bookmark)
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @list = @bookmark.list
+    @bookmark.destroy
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # resources :movies
+  resources :lists do
+    resources :bookmarks
+  end
+  resources :bookmarks
 end
